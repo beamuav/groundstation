6d697c3088c7cf366ec4cdfc3b0f494a5e0bd579
Josh Price <josh@alembic.com.au>
2019-08-07 00:03:31 +1000

    Add artificial horizon widget

--- lib/live_view_demo_web/live/compass_live.ex
+++ lib/live_view_demo_web/live/compass_live.ex
@@ -3,32 +3,32 @@ defmodule LiveViewDemoWeb.CompassLive do
 
   def render(assigns) do
     ~L"""
-    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <rect fill="none" height="100" width="100" x="0" y="0" stroke="#eee" stroke-width="0.5" />
-      <text font-size="5" font-weight="bold" x="50.5" y="7" text-anchor="middle"><%= assigns.heading %>º</text>
-      <g transform="rotate(<%= assigns.heading %>)" transform-origin="50 50">
-        <circle cx="50" cy="50" r="40" fill="whitesmoke" />
-        <circle cx="50" cy="50" r="38" fill="none" stroke="#aaa" stroke-width="4" stroke-dasharray="0.25,4.72" stroke-dashoffset="0" />
-        <circle cx="50" cy="50" r="38" fill="none" stroke="#aaa" stroke-width="4" stroke-dasharray="0.5,14.395" stroke-dashoffset="0" />
-        <circle cx="50" cy="50" r="37" fill="none" stroke="#666" stroke-width="6" stroke-dasharray="0.75,57.4" stroke-dashoffset="29.3" />
-        <circle cx="50" cy="50" r="37" fill="none" stroke="#111" stroke-width="6" stroke-dasharray="1,57.15" stroke-dashoffset="0.4" />
+    <svg viewBox="-50 -50 100 100" xmlns="http://www.w3.org/2000/svg">
+      <rect fill="none" height="100" width="100" x="-50" y="-50" stroke="#eee" stroke-width="0.5" />
+      <text font-size="5" font-weight="bold" x="0.5" y="7" text-anchor="middle"><%= assigns.heading %>º</text>
+      <g transform="rotate(<%= assigns.heading %>)">
+        <circle  r="40" fill="whitesmoke" />
+        <circle  r="38" fill="none" stroke="#aaa" stroke-width="4" stroke-dasharray="0.25 4.72" stroke-dashoffset="0" />
+        <circle  r="38" fill="none" stroke="#aaa" stroke-width="4" stroke-dasharray="0.50 14.395" stroke-dashoffset="0" />
+        <circle  r="37" fill="none" stroke="#666" stroke-width="6" stroke-dasharray="0.75 57.4" stroke-dashoffset="29.3" />
+        <circle  r="37" fill="none" stroke="#111" stroke-width="6" stroke-dasharray="1.00 57.15" stroke-dashoffset="0.4" />
         <g id="cardinal">
-          <text font-size="5" font-weight="bold" x="50" y="21" text-anchor="middle" transform="rotate(0)" transform-origin="50 50">N</text>
-          <text font-size="5" font-weight="bold" x="50" y="21" text-anchor="middle" transform="rotate(90)" transform-origin="50 50">E</text>
-          <text font-size="5" font-weight="bold" x="50" y="21" text-anchor="middle" transform="rotate(180)" transform-origin="50 50">S</text>
-          <text font-size="5" font-weight="bold" x="50" y="21" text-anchor="middle" transform="rotate(270)" transform-origin="50 50">W</text>
+          <text font-size="5" font-weight="bold" x="0" y="-29" text-anchor="middle" transform="rotate(0)">N</text>
+          <text font-size="5" font-weight="bold" x="0" y="-29" text-anchor="middle" transform="rotate(90)">E</text>
+          <text font-size="5" font-weight="bold" x="0" y="-29" text-anchor="middle" transform="rotate(180)">S</text>
+          <text font-size="5" font-weight="bold" x="0" y="-29" text-anchor="middle" transform="rotate(270)">W</text>
         </g>
         <g id="primary-intercardinal">
-          <text font-size="5" font-weight="normal" x="50" y="21" text-anchor="middle" transform="rotate(45)" transform-origin="50 50">NE</text>
-          <text font-size="5" font-weight="normal" x="50" y="21" text-anchor="middle" transform="rotate(135)" transform-origin="50 50">SE</text>
-          <text font-size="5" font-weight="normal" x="50" y="21" text-anchor="middle" transform="rotate(225)" transform-origin="50 50">SW</text>
-          <text font-size="5" font-weight="normal" x="50" y="21" text-anchor="middle" transform="rotate(315)" transform-origin="50 50">NW</text>
+          <text font-size="5" font-weight="normal" x="0" y="-29" text-anchor="middle" transform="rotate(45)">NE</text>
+          <text font-size="5" font-weight="normal" x="0" y="-29" text-anchor="middle" transform="rotate(135)">SE</text>
+          <text font-size="5" font-weight="normal" x="0" y="-29" text-anchor="middle" transform="rotate(225)">SW</text>
+          <text font-size="5" font-weight="normal" x="0" y="-29" text-anchor="middle" transform="rotate(315)">NW</text>
         </g>
       </g>
       <g>
-        <circle cx="50" cy="50" r="1.5" fill="none" stroke="red" stroke-width="0.5" />
-        <circle cx="50" cy="50" r="1" fill="#333" />
-        <line x1="50" y1="48.5" x2="50" y2="12" fill="none" stroke="red" stroke-width="0.5" stroke-linecap="round" />
+        <circle r="1.5" fill="none" stroke="red" stroke-width="0.5" />
+        <circle r="1" fill="#333" />
+        <line x1="0" y1="-1.5" x2="0" y2="-40" fill="none" stroke="red" stroke-width="0.5" stroke-linecap="round" />
       </g>
     </svg>
     """
--- lib/live_view_demo_web/live/horizon_live.ex
+++ lib/live_view_demo_web/live/horizon_live.ex
@@ -3,48 +3,96 @@ defmodule LiveViewDemoWeb.HorizonLive do
 
   def render(assigns) do
     ~L"""
-    <svg viewBox="-50 -50 50 50" xmlns="http://www.w3.org/2000/svg">
-      <rect fill="none" height="100" width="100" x="0" y="0" stroke="#eee" stroke-width="0.5" />
-      <text font-size="5" font-weight="bold" x="0.5" y="7" text-anchor="middle"><%= assigns.heading %>º</text>
-      <g transform="rotate(<%= assigns.heading %>)">
-        <circle  r="40" fill="whitesmoke" />
-        <circle  r="38" fill="none" stroke="#aaa" stroke-width="4" stroke-dasharray="0.25,4.72" stroke-dashoffset="0" />
-        <circle  r="38" fill="none" stroke="#aaa" stroke-width="4" stroke-dasharray="0.5,14.395" stroke-dashoffset="0" />
-        <circle  r="37" fill="none" stroke="#666" stroke-width="6" stroke-dasharray="0.75,57.4" stroke-dashoffset="29.3" />
-        <circle  r="37" fill="none" stroke="#111" stroke-width="6" stroke-dasharray="1,57.15" stroke-dashoffset="0.4" />
-        <g id="cardinal">
-          <text font-size="5" font-weight="bold" x="0" y="21" text-anchor="middle" transform="rotate(0)">N</text>
-          <text font-size="5" font-weight="bold" x="0" y="21" text-anchor="middle" transform="rotate(90)">E</text>
-          <text font-size="5" font-weight="bold" x="0" y="21" text-anchor="middle" transform="rotate(180)">S</text>
-          <text font-size="5" font-weight="bold" x="0" y="21" text-anchor="middle" transform="rotate(270)">W</text>
+    <svg viewBox="-50 -50 100 100" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <linearGradient id="sky" x2="0%" y2="100%">
+          <stop offset="0%" stop-color="darkblue" />
+          <stop offset="100%" stop-color="skyblue" />
+        </linearGradient>
+        <linearGradient id="ground" x2="0%" y2="100%">
+          <stop offset="0%" stop-color="sienna" />
+          <stop offset="100%" stop-color="darkbrown" />
+        </linearGradient>
+        <radialGradient id="circle-fade-mask" x2="0%" y2="100%">
+          <stop offset="0%" stop-color="transparent" stop-opacity="0" />
+          <stop offset="60%" stop-color="transparent" stop-opacity="0.8" />
+          <stop offset="100%" stop-color="black" stop-opacity="0.9" />
+        </radialGradient>
+      </defs>
+      <rect fill="none" height="100" width="100" x="-50" y="-50" stroke="#eee" stroke-width="0.5" />
+
+      <g transform="rotate(<%= @roll_angle %>)">
+        <g transform="translate(0 <%= @pitch_angle %>)">
+          <rect fill="url(#sky)" height="200" width="200" x="-100" y="-200" />
+          <rect fill="url(#ground)" height="200" width="200" x="-100" y="0" stroke="white" stroke-width="0.25" />
+
+          <g id="pitch-tape">
+            <g id="pitch-labels" fill="white" font-size="3" text-anchor="middle">
+              <text x="-20" y="-20" alignment-baseline="middle">20</text>
+              <text x="20" y="-20" alignment-baseline="middle">20</text>
+              <text x="-20" y="-10" alignment-baseline="middle">10</text>
+              <text x="20" y="-10" alignment-baseline="middle">10</text>
+              <text x="-20" y="10" alignment-baseline="middle">10</text>
+              <text x="20" y="10" alignment-baseline="middle">10</text>
+              <text x="-20" y="20" alignment-baseline="middle">20</text>
+              <text x="20" y="20" alignment-baseline="middle">20</text>
+            </g>
+
+            <g id="pitch-angle-lines" stroke="white">
+              <line x1="-15" y1="-20" x2="15" y2="-20" stroke-width="0.5"/>
+              <line x1="-15" y1="-10" x2="15" y2="-10" stroke-width="0.5"/>
+              <line x1="-15" y1="10" x2="15" y2="10" stroke-width="0.5"/>
+              <line x1="-15" y1="20" x2="15" y2="20" stroke-width="0.5"/>
+              <line x1="-10" y1="-15" x2="10" y2="-15" stroke-width="0.25"/>
+              <line x1="-10" y1="-5" x2="10" y2="-5" stroke-width="0.25"/>
+              <line x1="-10" y1="5" x2="10" y2="5" stroke-width="0.25"/>
+              <line x1="-10" y1="15" x2="10" y2="15" stroke-width="0.25"/>
+            </g>
+
+            <!-- <circle id="mask" r="40" fill="url(#circle-fade-mask)" /> -->
+          </g>
         </g>
-        <g id="primary-intercardinal">
-          <text font-size="5" font-weight="normal" x="0" y="21" text-anchor="middle" transform="rotate(45)">NE</text>
-          <text font-size="5" font-weight="normal" x="0" y="21" text-anchor="middle" transform="rotate(135)">SE</text>
-          <text font-size="5" font-weight="normal" x="0" y="21" text-anchor="middle" transform="rotate(225)">SW</text>
-          <text font-size="5" font-weight="normal" x="0" y="21" text-anchor="middle" transform="rotate(315)">NW</text>
-        </g>
+        <path id="pitch-pointer" d="M 0,-45 L -1.5,-42 L 1.5,-42 Z" fill="white" />
       </g>
-      <g>
-        <circle r="1.5" fill="none" stroke="red" stroke-width="0.5" />
-        <circle r="1" fill="#333" />
-        <line x1="0" y1="48.5" x2="50" y2="12" fill="none" stroke="red" stroke-width="0.5" stroke-linecap="round" />
+
+      <g id="pitch-angle">
+        <path id="pitch-arc" d="M-45,0 A5,5 0 1,1 45,0" fill="none" stroke="white" stroke-width="0.5" />
+        <path id="pitch-ticks" d="M-44,0 A5,5 0 1,1 44,0" fill="none" stroke="white" stroke-width="2" stroke-dasharray="0.25,7.41" stroke-dashoffset="0" />
+        <path id="pitch-pointer" d="M 0,-45 L -1.5,-48 L 1.5,-48 Z" fill="white" />
+      </g>
+
+      <g id="level-indicator" stroke="yellow" stroke-width="1.5" stroke-linecap="round">
+        <line x1="-45" y1="0" x2="-25" y2="0" />
+        <line x1="45" y1="0" x2="25" y2="0" />
+        <line x1="-5" y1="4" x2="0" y2="0" stroke-linecap="square"  stroke-width="1" />
+        <line x1="5" y1="4" x2="0" y2="0" stroke-linecap="square" stroke-width="1" />
       </g>
     </svg>
+    <form phx-change="update">
+      <fieldset>
+        <label>Roll</label>
+        <input type="range" min="-180" max="180" name="roll_angle" value="<%= @roll_angle %>" />
+        <%= @roll_angle %>º
+      </fieldset>
+      <fieldset>
+        <label>Pitch</label>
+        <input type="range" min="-30" max="30" name="pitch_angle" value="<%= @pitch_angle %>" />
+        <%= @pitch_angle %>º
+      </fieldset>
+    </form>
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(250, self(), :tick)
-
-    {:ok, put_heading(socket, 0)}
+    {:ok, put_attitude(socket, 0, 0)}
   end
 
-  def handle_info(:tick, socket) do
-    {:noreply, put_heading(socket, socket.assigns.heading + 1)}
+  def handle_event("update", %{"roll_angle" => roll_angle, "pitch_angle" => pitch_angle}, socket) do
+    {:noreply,
+     put_attitude(socket, String.to_integer(roll_angle), String.to_integer(pitch_angle))}
   end
 
-  defp put_heading(socket, heading) do
-    assign(socket, heading: rem(heading, 360))
+  defp put_attitude(socket, roll_angle, pitch_angle) do
+    assign(socket, roll_angle: rem(roll_angle, 360), pitch_angle: rem(pitch_angle, 360))
   end
 end
