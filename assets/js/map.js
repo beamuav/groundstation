3e3098d780fa08cde11d7faaba82ed230cb6d6c2
Josh Price <josh@alembic.com.au>
2019-08-10 17:49:49 +1000

    Add Dalby model aero club coords

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,20 +1,20 @@
 import L from "leaflet";
 
-const map = L.map("map").setView([-27.15852, 151.26337], 13);
+const map = L.map("map").setView([-27.272292, 151.282036], 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-27.15852, 151.26337])
+L.marker([-27.272292, 151.282036])
   .addTo(map)
-  .bindPopup("Ground Control<br/>to Major Tom").openPopup();
+  .bindPopup("Ground Control<br/>to Major Tom");
 
 const polygon = L.polygon(
   [
-    [-27.15852, 151.29],
-    [-27.14, 151.27],
-    [-27.17, 151.25]
+    [-27.27, 151.28],
+    [-27.28, 151.20],
+    [-27.24, 151.25]
   ], {
     color: "red",
     fillColor: "#f03",
