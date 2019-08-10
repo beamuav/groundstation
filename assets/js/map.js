2209f8e72302caad69d501ec92bc49421892c56a
Josh Price <josh@alembic.com.au>
2019-08-10 13:01:32 +1000

    Add an exclusion zone

--- assets/js/map.js
+++ assets/js/map.js
@@ -9,5 +9,14 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 L.marker([-27.15852, 151.26337])
   .addTo(map)
-  .bindPopup("Ground Control<br/>to Major Tom")
-  .openPopup();
+  .bindPopup("Ground Control<br/>to Major Tom");
+
+const polygon = L.polygon(
+  [[-27.15852, 151.29], [-27.14, 151.27], [-27.17, 151.25]],
+  {
+    color: "red",
+    fillColor: "#f03",
+    weight: 0.75,
+    fillOpacity: 0.1
+  }
+).addTo(map);
