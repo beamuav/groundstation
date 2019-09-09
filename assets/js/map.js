f3d42b31071f91bc313a4ac04785904f9fae62d6
Josh Price <josh@alembic.com.au>
2019-09-09 22:21:20 +1000

    Upgrade to latest release of live view

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,19 +6,17 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-27.272292, 151.282036])
-  .addTo(map)
-  .bindPopup("Ground Control<br/>to Major Tom");
+L.marker([-27.272292, 151.282036]).addTo(map)
 
-const polygon = L.polygon(
-  [
-    [-27.27, 151.28],
-    [-27.28, 151.20],
-    [-27.24, 151.25]
-  ], {
-    color: "red",
-    fillColor: "#f03",
-    weight: 0.75,
-    fillOpacity: 0.1
-  }
-).addTo(map);
+// const polygon = L.polygon(
+//   [
+//     [-27.27, 151.28],
+//     [-27.28, 151.20],
+//     [-27.24, 151.25]
+//   ], {
+//     color: "red",
+//     fillColor: "#f03",
+//     weight: 0.75,
+//     fillOpacity: 0.1
+//   }
+// ).addTo(map);
